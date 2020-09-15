5e3db7a24db7fc211f8d0b6fcdb0912128142663
mattygrecgrec <35116823+mattygrecgrec@users.noreply.github.com>
2020-09-15 07:49:11 -0400

    Update background.js
    working fetch (but not doing anything with it yet)

--- background.js
+++ background.js
@@ -1,27 +1,56 @@
-const setList = (results) => {
-    
+//define a few logging functions we're going to need later
+const examineIt = (stuff) => {
+    console.log(stuff)
+    return stuff;
+}
+
+const dealWithResponse = (stuff) => {
+    let info = stuff.json()
+    console.log('The expert info response is   ' + info)
+    return info;
+}
+
+const checkHttp = (promise) => {
+    console.log('the status of the request is  ' + promise.status)
+    if (promise.status !== 200) {
+       // alert('uh oh...problem getting the expert info back ' + promise.status);
+    }
+    return promise;
+}
+
+function makeUL(array) {
+    // Create the list element:
+    var list = document.createElement('ul');
+    for(var i = 0; i < array.length; i++) {
+        // Create the list item:
+        var item = document.createElement('li');
+        // Set its contents:
+        item.appendChild(document.createTextNode(array[i]));
+        // Add it to the list:
+        list.appendChild(item);
+    }
+    // Finally, return the constructed list:
+    return list;
 }
 
 
+
+
+
+//This is what we do when the button is clicked.  First we fetch the data from our API then we create a table baded off the results.
 const getData = (buttonResults) => {
     fetch("https://izas4pssoe.execute-api.us-east-1.amazonaws.com/staging", {
       "method": "POST",
-        "mode": "no-cors",
       "headers": {
         "content-type": "text/plain"
       },
-      "body": {
-        "artists": [
-          "James Joyce",
-          "Bob Dylan"
-        ],
-        "dob": "1986-10-10"
-      }
+      "body": JSON.stringify(
+         {'artists': ['James Joyce', 'Bob Dylan'], 'dob': '1986-10-10'}
+      )
     })
-    .then(response => {
-      console.log(response);
-    })
-    .then(setList)
+    .then(checkHttp)
+    .then(dealWithResponse)
+    .then(examineIt)
     .catch(err => {
       console.error(err);
     });
